9c2ad30f466c73822a7e12aeacd46aa63b98b935
Glenardd <tolentinojanglenard5@gmail.com>
2026-03-12 22:19:21 +0800

    fixed services

--- services/test.service.ts
+++ services/test.service.ts
@@ -1,27 +1,34 @@
 import { supabase } from "@/lib/supabase";
+import { GetTestTypes, InsertTestTypes } from "@/utils/dataTypes";
+
+//current user or authenticated user
+export async function getCurrentUser() {
+  const { data: { user } } = await supabase.auth.getUser();
+  return user;
+};
 
 // Get all tests
-export async function getTest(){
+export async function getTest(): Promise<GetTestTypes[]>{
    const { data, error } = await supabase.from("test").select("*");
    if (error) {
        console.error("Error fetching tests:", error);
        return [];
    }
    return data;
-}
+};
 
 // delete tests
-export async function deleteTest(id: string){
-    const { error } = await supabase.from("tests").delete().eq("id", id);
+export async function deleteTest(id: number){
+    const { error } = await supabase.from("test").delete().eq("id", id);
     if (error) {
         console.error("Error deleting test:", error);
-    }
-}
+    };
+};
 
 // insert tests
-export async function createTest(data: any){
-    const { error } = await supabase.from("tests").insert(data);
+export async function createTest(data: InsertTestTypes){
+    const { error } = await supabase.from("test").insert(data).select().single();
     if (error) {
         console.error("Error creating test:", error);
-    }
-}
+    };
+};
